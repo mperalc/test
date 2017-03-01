635b081833bc96318dc8d0831fd6e8031e19794f
mperalc <martac@well.ox.ac.uk>
2017-03-01 12:16:12 +0000

    tidy

--- merge_DEA_peaks_and_across_stages_results.R
+++ merge_DEA_peaks_and_across_stages_results.R
@@ -1,15 +1,11 @@
-# Making venn diagrams and mergin DEA tables with peak and across-stages results
+# Making venn diagrams and merging DEA tables with peak and across-stages results
 
 #load libraries
 library(VennDiagram)
 
-
-
 stage= c("iPSC", "DE", "PGT", "PFG", "PE", "EP","EN6", "EN7") # 8 stages
 
 currentDate <- Sys.Date() # to save date in name of output files
-
-
 
 ######### Read in data
 
@@ -18,28 +14,19 @@ currentDate <- Sys.Date() # to save date in name of output files
 DE_stage_specific<-list()
 DE_across_stages<-list()
 
-DE_merged<-list()  # for final tables
-
 #### the following might require changes
 
-
 for(i in stage){
-  
-  
-  DE_stage_specific[[i]] <- read.csv(file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/conservative_counts/2016-10-17_sig_maxvals_",i,"_diff_expression_results_logFC1.csv",sep=""))
+  #peak
+  DE_stage_specific[[i]] <- read.csv(file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/stage-specific/2017-02-28_sig_maxvals_",i,"_diff_expression_results_logFC1.csv",sep=""))
   colnames(DE_stage_specific[[i]]) <- c("ensembl_gene_id", "external_gene_name","gene_biotype", "chromosome_name","logFC","adj.P.Val")
+  
   # get contrast matrix results
-  
-  
-  DE_across_stages[[i]] <- read.csv(file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/conservative_counts/Contrasts_as_Martijn/2016-10-17_sig_",i,"_diff_expression_maxvals_contrasts_results_logFC1.csv",sep=""))
-  
-}
-
+    DE_across_stages[[i]] <- read.csv(file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/across-stages/2017-03-01_sig_",i,"_diff_expression_maxvals_across-stages_results_logFC1.csv",sep=""))
 
   # make list that includes: ensembl_ID, gene, gene_biotype, chr,logFC_peak, logFC_timecourse, adjust.pval_peak, adjust.pval_timecourse, sig_peak/timecourse
   
   try = merge(DE_stage_specific[[i]][c(1:6)],DE_across_stages[[i]][c(1:6)],by=c("ensembl_gene_id","external_gene_name","gene_biotype","chromosome_name"),all=TRUE)
-  
   try=try[c(1,2,3,4,5,7,6,8)]
   colnames(try)=c("ensembl_gene_id","gene_name","gene_biotype","chr","logFC_peak","logFC_timecourse","adjust.pval_peak","adjust.pval_timecourse")
   
@@ -53,19 +40,11 @@ for(i in stage){
   try[which(is.na(try[7]) & !is.na(try[8])),9] <- "timecourse"
   colnames(try)[9]<-"sig_peak/timecourse"
   
-  DE_merged[[i]] <- try  # append to list of dataframes
-  
-  
   
   write.csv(try,quote=F,row.names=F,
-            file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/conservative_counts/",
+            file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/",
                        currentDate,"_sig_", i,"_diff_expression_maxvals_stage-unique_and_across-stages_logFC1.csv",sep=""))
   
 }
-
-# save DE_merged as combined dataframe or as object?
-
-
-
 
 
