7fc005d5e1834e2655a05b6fb06c93253991eb75
mperalc <martac@well.ox.ac.uk>
2017-03-04 20:24:00 +0000

    change design for normalization

--- DE_old_vs_new_limma_voom.R
+++ DE_old_vs_new_limma_voom.R
@@ -38,17 +38,67 @@ design=cbind(design[,c(1,12,5)],c(rep(0,3),rep(1,3),rep(0,30)),design[,c(8,7,6,4
 colnames(design)[4] <- "stagesDE"
 
 
+########## 1st method
+# 
+# design2=design[,c(1:2,9)]   # EN6
+# 
+# v2=voom(filtered_combined_commongenes,design=design2,save.plot=TRUE) # voom normalize before fitting the linear model
+# 
+# fit2 <- lmFit(v2,design2)
+# fit2 <- eBayes(fit2)
+# 
+# diff_exp=topTable(fit2,coef=ncol(design2),sort.by = "none",number=nrow(fit2$coefficients))  
+# 
+# diff_exp_sig=diff_exp[which(diff_exp$adj.P.Val<0.01),]  #pval<0.01
 
-########## continue from here
+
+###### 2nd method:
 
 
-design2=design[,c(1,12,2)]
+diff_exp_allstages <- list()
+diff_exp_allstages_nominal <- list()
+for(s in old_stages){
+  
+  v2=voom(filtered_combined_commongenes,design=design,save.plot=TRUE) 
+  rownames(design)=colnames(v2$E)   # to select rows more easily
+  v3=v2[,grepl( s , colnames( v2) )]  # take only columns from selected stage
+  
+  design3=design[grepl( s , rownames( design) ),c(1,2)]  # same with design matrix
+  
+  fit3 <- lmFit(v3,design3)
+  fit3 <- eBayes(fit3)
+  
+  diff_exp3=topTable(fit3,coef=ncol(design3),sort.by = "none",number=nrow(fit3$coefficients))  
+  diff_exp_allstages_nominal[[s]]=diff_exp3[which(diff_exp3$P.Value<0.05 & (diff_exp3$logFC>1 | diff_exp3$logFC<(-1))),]
+  diff_exp_allstages[[s]]=diff_exp3[which(diff_exp3$adj.P.Val<0.05 & (diff_exp3$logFC>1 | diff_exp3$logFC<(-1))),]  #pval<0.01 and logFC>1
+  write.csv(diff_exp_allstages[[s]],file=paste("/Users/Marta/Documents/WTCHG/DPhil/Data/Results/Diff_v2/Voom/comparison_old_and_new_study/",currentDate,"_diff_exp_old_vs_new_protocol_",s,"_stage_5percent_pval.csv",sep=""),
+            row.names = F)
+  
+}
 
-v2=voom(filtered_combined_commongenes,design=design2,save.plot=TRUE) # voom normalize before fitting the linear model
+# increasing number of significant differences between new and old expression, except for PE
+# maybe due to less expressed genes?
 
-fit2 <- lmFit(v2,design2)
-fit2 <- eBayes(fit2)
+# counting number of genes in each stage with expression >1cpm in at least two samples
+zero_counts_percent = list()
+for(s in old_stages){
+  stage=cpm(filtered_combined_commongenes$counts)  #takes columns whose name matches x
+  stage=stage[ , grepl( s , colnames( stage) ) ]
+  stage[stage>=1]=1 # convert to table where any value above 1 cpm = 1
+  stage[stage<1]=0 # convert to table where any value below 1 cpm = 0
+  
+  sum_of_each_row=rowSums(stage)
+  zero_counts_percent[[s]]=100*(sum(sum_of_each_row<3)/length(sum_of_each_row)) # % of zero counts in at least 2 stages
+}
 
-diff_exp=topTable(fit2,coef=ncol(design2),sort.by = "none",number=nrow(fit2$coefficients))  
+# doesn't seem like PE has more 0 counts than others, but there are less differentially expressed genes than the average of other stages, 
+# specially with the stage-specific method 
 
-diff_exp_sig=diff_exp[which(diff_exp$adj.P.Val<0.01),]  #pval<0.01
+
+# how many logFC are negative? (that means that old exp has increased expression compared to new stage)
+is_negative <- list()
+for(s in old_stages){
+  is_negative[[s]]=table(diff_exp_allstages[[s]]$logFC<0)
+}
+
+
